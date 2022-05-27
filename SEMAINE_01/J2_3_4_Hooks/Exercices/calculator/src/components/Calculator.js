eed6dfa747db856cfca46987964c30abc8a65da0
Antoine07 <antoine.lucsko@gmail.com>
2022-05-27 12:10:52 +0200

    calculator without message

--- SEMAINE_01/J2_3_4_Hooks/Exercices/calculator/src/components/Calculator.js
+++ SEMAINE_01/J2_3_4_Hooks/Exercices/calculator/src/components/Calculator.js
@@ -1,15 +1,25 @@
-
 import { useReducer } from 'react';
-
-import {initialState, reducer, COUNT } from '../store/reducer';
+import {initialState, reducer, COUNT, SET_NUMBER, ADD, MULT } from '../store/reducer';
 
 function Calculator() {
     const [state, dispatch ] = useReducer(reducer, initialState);
-    console.log(state.count)
+    const { number1, number2, resultat} = state;
+
+    function handleChange(e){
+        const { name, value: number } = e.target;
+        dispatch({type : SET_NUMBER, name, number });
+    }
+
   return (
     <div className="App">
       <p>Count : {state.count}</p>
       <p><button onClick={() => dispatch({type : COUNT, step : 2 })}> + 2</button></p>
+      <h1>Calculator</h1>
+      { resultat !== null && (<p>{resultat}</p>)}
+        <p>Number 1 :<input onChange={handleChange} type="text" name="number1" value={number1} /></p>
+        <p>Number 2 :<input onChange={handleChange} type="text" name="number2" value={number2} /></p>
+        <p><button onClick={() => dispatch({type : ADD })}>ADD</button></p>
+        <p><button onClick={() => dispatch({type : MULT })}>ADD</button></p>
     </div>
   );
 }
--- SEMAINE_01/J2_3_4_Hooks/Exercices/calculator/src/store/reducer.js
+++ SEMAINE_01/J2_3_4_Hooks/Exercices/calculator/src/store/reducer.js
@@ -1,14 +1,16 @@
 
 // state <=> la source de vérité 
 export const initialState = {
-    number: '',
-    resultat: 0,
-    count : 0
+    number1: '',
+    number2: '',
+    resultat: null,
+    count: 0
 };
 
 export const MULT = 'mult';
 export const ADD = 'add';
 export const COUNT = 'count';
+export const SET_NUMBER = 'SET_NUMBER';
 
 export const reducer = (state, action) => {
 
@@ -18,15 +20,36 @@ export const reducer = (state, action) => {
                 ...state
             };
         case ADD:
-            return {
-                ...state
-            };
-        case COUNT:
-            console.log(action);
+            const n1 = parseFloat(state.number1) ;
+            const n2 = parseFloat(state.number2) ;
+
+            if(isNaN(n1) || isNaN(n2)){
+
+                return {
+                    ...state
+                }
+            }
+
             return {
                 ...state,
-                count : state.count + action.step
-            }
+                resultat : n1 + n2,
+                number1 : '',
+                number2 : ''
+            };
+        case COUNT:
+            return {
+                ...state,
+                count: state.count + action.step
+            };
+        case SET_NUMBER:
+            const { name, number } = action;
+
+            return {
+                ...state,
+                [name]: number,
+                resultat : null
+            };
+
         default:
             return state;
     }
